c4bcd75eaea1d42bf1c0ebabb37a6d5d78f6a907
xBuw <kirichenko_s@bk.ru>
2017-03-15 18:07:39 +0200

    Initial commit

---
+++ src/Application.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xbuw
+ * Date: 15.03.17
+ * Time: 17:20
+ */
+
+namespace xbuw\framework;
+
+
+class Application
+{
+    function __construct()
+    {
+        echo "hello application</br>";
+    }
+}
