aabd188c2e8a56031ded370afe3ab2b079b5defb
xBuw <kirichenko_s@bk.ru>
2017-03-17 11:19:18 +0200

    Add render layout to Controller

--- src/Controller/Controller.php
+++ src/Controller/Controller.php
@@ -14,9 +14,20 @@ use xbuw\framework\Response\Response;
 
 class Controller
 {
-    public function render($viewPath, $params = []):Response
+    /**
+     * @param string $viewPath
+     * @param array $params
+     * @param bool $layout true if exist layout
+     * @return Response
+     */
+    public function render(string $viewPath, array $params = [], bool $layout = false):Response
     {
         $content = Renderer::render($viewPath, $params);
+        if($layout){
+            $pathInfo = pathinfo($viewPath);
+            $layoutPath = $pathInfo['dirname'] .'/layout.' . $pathInfo['basename'];
+            $content = Renderer::render($layoutPath, ['content' => $content]);
+        }
         return new Response($content);
     }
 }
